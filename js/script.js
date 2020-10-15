a0c1cc62e7c7ed34d0d0022b11002b7652be7b59
Psarvesh1 <sarveshparab602@gmail.com>
2020-10-15 14:02:08 +0530

    tweaks in responsive

--- js/script.js
+++ js/script.js
@@ -21,19 +21,19 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = '../css/default.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = '../css/blue.css'
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById('theme-style').href = '../css/green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById('theme-style').href = '../css/purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
